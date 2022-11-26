f164fb708d6e3e7468a24681099ae761aa607a0f
samsnwn <samuel.c.glantz@gmail.com>
2022-11-26 14:37:31 +0100

    checking smallest phones

--- src/components/BackgroundCircles.jsx
+++ src/components/BackgroundCircles.jsx
@@ -4,7 +4,7 @@ import styles from '../style'
 
 const BackgroundCircles = () => {
   return (
-    <motion.div className='relative top-10 md:top-0 flex justify-center items-center' initial={{opacity:0 }} animate={{scale:[1,2,2,3,1], rotate:360,opacity:[0.1, 0.2, 0.4, 0.8, 0.1, 1.0]}} transition={{duration:2.5}}>
+    <motion.div className='relative top-20 md:top-0 flex justify-center items-center' initial={{opacity:0 }} animate={{scale:[1,2,2,3,1], rotate:360,opacity:[0.1, 0.2, 0.4, 0.8, 0.1, 1.0]}} transition={{duration:2.5}}>
         <div className="absolute border border-[#333333] rounded-full h-[100px] w-[100px] min-[600px]:h-[200px]  min-[600px]:w-[200px] mt-52 animate-ping "/>
         <div className="absolute border border-[#333333] rounded-full h-[150px] w-[150px] min-[600px]:h-[300px]  min-[600px]:w-[300px] mt-52 "/>
         <div className="absolute border border-[#333333] rounded-full h-[250px] w-[250px] min-[600px]:h-[500px]  min-[600px]:w-[500px] mt-52 "/>
