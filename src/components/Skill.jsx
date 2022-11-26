0255c09ee221923458aeb2e59c8b60ff49427798
samsnwn <samuel.c.glantz@gmail.com>
2022-11-26 19:02:05 +0100

    checking smaller devices

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -9,8 +9,8 @@ const Skill = () => {
       {skills.map((skill, i) => (
         <div key={i}></div>
       ))}
-      <div
-        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+      <motion.div
+        initial={{ x: directionLeft ? -100 : 100, opacity: 0 }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         className="rounded-full border border-gray-500 object-cover w-16 h-16 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
@@ -18,7 +18,7 @@ const Skill = () => {
       <div className="absolute rounded-full opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16
        md:w-28 md:h-28 xl:h-32 xl:w-32 z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+          <p className="text-lg md:text-3xl font-bold text-black opacity-100">100%</p>
         </div>
       </div>
     </div>
